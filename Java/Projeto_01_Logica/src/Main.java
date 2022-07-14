ea0cc5c5791cb8d294d28a2e59b984b68855f5f3
l0p0v <4drade@gmail.com>
2022-07-14 19:50:24 -0300

    last commit
    without continuity

--- Java/Projeto_01_Logica/src/Main.java
+++
@@ -1,7 +0,0 @@
-import com.dankicode.utilidades.Utils;
-public class Main {
-    public static void main(String[] args) {
-        Utils utils = new Utils();
-        utils.idade = 9;
-    }
-}
--- Java/Projeto_02_Logica/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String x = "Ola mundo!";
-        x += "\nBoa noite.";
-        System.out.println(x);
-
-    }
-}
